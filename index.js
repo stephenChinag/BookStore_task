af44e88e25031d5ae341bf74054c76565e5c89aa
StpehenChinag <stephenchinag@gmail.com>
2024-01-26 18:58:33 +0100

    fixed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send("Welcome To MERN Stack Tutorial");
+  return response.status(234).send("My Book Task");
 });
 
 app.use("/books", booksRoute);
